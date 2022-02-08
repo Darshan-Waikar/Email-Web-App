77f1e96618f60d9dc9f5b3d079bcfe8a5369b206
Darshan <waikardarshan@gmail.com>
2022-02-08 19:07:54 +0530

    Minor Change

--- src/App.js
+++ src/App.js
@@ -14,9 +14,8 @@ class App extends Component {
     sendEmail = (_) => {
         const { email } = this.state;
         fetch(
-                `http://127.0.0.1:4000/send-email?recipient=${email.recipient}&sender=${email.sender}&topic=${email.subject}&text=${email.text}`
-            ) //query string url
-            .catch((err) => console.error(err));
+            `http://127.0.0.1:4000/send-email?recipient=${email.recipient}&sender=${email.sender}&topic=${email.subject}&text=${email.text}`
+        ).catch((err) => console.error(err));
     };
 
     render() {
