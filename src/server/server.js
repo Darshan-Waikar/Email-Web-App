b07099f3227daffa4f3f0f010a6ff91d1ea1be57
Darshan <waikardarshan@gmail.com>
2022-02-08 18:53:45 +0530

    Minor Changes

--- src/server/server.js
+++ src/server/server.js
@@ -7,18 +7,17 @@ sgMail.setApiKey(
     "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"
 );
 
-app.use(cors()); //utilize Cors so the browser doesn't restrict data, without it Sendgrid will not send!
+app.use(cors());
 
-// Welcome page of the express server:
+
 app.get("/", (req, res) => {
     res.send("Welcome to the Sendgrid Emailing Server");
 });
 
 app.get("/send-email", (req, res) => {
-    //Get Variables from query string in the search bar
+
     const { recipient, sender, topic, text } = req.query;
 
-    //Sendgrid Data Requirements
     const msg = {
         to: recipient,
         from: sender,
@@ -26,9 +25,9 @@ app.get("/send-email", (req, res) => {
         text: text,
     };
 
-    //Send Email
+
     sgMail.send(msg).then((msg) => console.log(text));
 });
 
-// to access server run 'nodemon index.js' then click here: http://localhost:4000/
+
 app.listen(4000, () => console.log("Running on Port 4000"));
